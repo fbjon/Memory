b9908a05b7a16b5d2c3c679a252228a160a5d3a3
Frej Bjon <fbjon@abo.fi>
2020-03-05 12:08:40 +0200

    Code review

--- script.js
+++ script.js
@@ -3,12 +3,30 @@ const messageDisplay = document.querySelector('#message');
 const resetButton = document.querySelector("#reset");
 const modeButtons = document.querySelectorAll(".mode");
 const bricks = document.querySelectorAll(".brick");
+/*
+Using the 'var' keyword is not a problem in itself, but be aware of the pitfalls. Consider the following (nonsensical) line
+at this point in the code:
+
+var abcd = numBricks + tries;
+
+Because 'tries' isn't defined until later, we wouldn't expect this to work, however it will run without error. 
+This is because the 'var' keyword causes the variable to exist as if it was defined at the top of the function
+(or file, in this case), but the contents of the variable is still undefined at this point, 'tries' won't be set to 0 
+until two lines below. Therefore the operation numBricks + tries is equivalent to 20 + undefined, which produces NaN.
+Using 'let' would produce an error instead, which almost always makes more sense. However, see caniuse.com/let
+*/
 var newPicArray;
 var tries = 0;
 var totalTries = 0;
 var firstGuess;
 var secondGuess;
 var header = document.querySelector('h1');
+
+/*
+Neat definition here. A different way to do it could be to have an array of just the picture names
+var pics = ["Albert.jpg", "Spiderman.jpg", ...]
+and from that construct the picArray. This separates the definitions from the implementation details.
+*/
 var picArray = [
     "url(pics/Albert.png)",
     "url(pics/Albert.png)",
@@ -48,7 +66,22 @@ var picArray = [
     "url(pics/Zebra.png)"
 ]
 
+/*
+This doesn't work as intended, although the bug might not cause problems in this case. The variable window.onload expects 
+to be assigned a function, however this line calls the function and assigns the return value to window.onload.
+Since setupBricks(12) returns nothing (i.e. undefined), this is the same as
+  window.onload = undefined;
+Normally you'd write window.onload = setupBricks; instead which assigns the function itself to the variable instead of calling it,
+but since setupBricks needs a parameter, you can do it like this:
+  window.onload = function() { setupBricks(12); };
+*/
 window.onload = setupBricks(12);
+/* 
+A second problem: Only one function can be assigned to .onload, so this line overwrites whatever was assigned on the previous line
+(though by chance it doesn't actually matter in this case). Consider combining the calls in one function, or using:
+  window.addEventListener("load", function(){ setupBricks(12) });
+  window.addEventListener("load", function(){ setupPics(12) }); 
+*/
 window.onload = setupPics(12);
 
 
@@ -79,6 +112,15 @@ resetButton.addEventListener('click', function () {
 //change the background of the clicked div, using the divs id, to corresponding image from randomized array of pics
 for (var i = 0; i < bricks.length; i++) {
     bricks[i].addEventListener('click', function () {
+        /*
+        This function is the entirety of the actual game. It does a few things which could be split out into
+        separate functions: game sequence, state logic, and UI code.
+        The game sequence and state logic is the overall behaviour of the game (no guess, first guess, second guess, game over),
+        as well as how the game moves from one state to another (user clicks something, reset buttons, etc.)
+        The UI code on the other hand is the styles, animations, and other visual details which don't affect the game itself.
+        By separating out these into other functions, this event listener becomes much shorter and easier to read, and more 
+        importantly, easier to test.
+        */
         //if brick is not yet clicked on and it's the first of the two tries, show picture from newPicArray
         if (!this.classList.contains('clicked') && tries <= 1 ) {
             this.style.transition = "background 0.6s"
@@ -129,6 +171,17 @@ function setupBricks(num) {
         brick.style.transition = "none"
         brick.style.background = "rgb(190, 190, 36)"
     })
+    /*
+    This function is simple and easy to read, but if there were more choices in number of bricks, it'd get longer and longer.
+    If you instead define a mapping like so:
+    var gameSizes = {
+        12: { display: "block, width: "22%", paddingBottom: "22%", margin: "1.5%" },
+        16: { ... },
+        ...
+    };
+    You would then only have to write one loop that handles all defined cases using that structure, instead of three different
+    loops as below.
+    */
     if (num === 12 || num === 16) {
         for (var i = 0; i < bricks.length; i++) {
             if (i < num) {
